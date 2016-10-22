cf82715f5ef5847e55716fe53be4f9dcae92815d
Diego Camargo <dicamargov@gmail.com>
2016-10-22 00:46:12 -0500

    Push project

---
+++ service.py
@@ -0,0 +1,39 @@
+from flask import Flask, json, request, render_template
+from invalid_usage import InvalidUsage
+
+app = Flask(__name__)
+
+info = []
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route("/info", methods = ['GET'])
+def read_info():
+  return json.jsonify(info)
+
+@app.route("/info", methods = ['POST'])
+def add_info():
+  try:
+    global info
+    data = map(float, request.data.split(","))
+    if len(data) != 3:
+      raise
+    info.append(data)
+    return read_info()
+  except Exception as e:
+    return json.jsonify(InvalidUsage('error').to_dict())
+
+@app.route("/info", methods = ['DELETE'])
+def delete_info():
+  global info
+  info = []
+  return read_info()
+
+@app.route("/visualize")
+def visualize():
+  return render_template("viz.html")
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
